e9daac52ce436aa691b19c8770b2232ffd28d23b
Iris Hu <ihu009@gmail.com>
2020-05-27 23:41:01 -0400

    Updates

---
+++ precache-manifest.c950e3bd39e0b02316fd3c651b3ca2fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a562f133128a88fa759c73a87f35932b",
+    "url": "/irishu/index.html"
+  },
+  {
+    "revision": "b3ca180c4dbf545727ba",
+    "url": "/irishu/static/css/main.e1fc00b0.chunk.css"
+  },
+  {
+    "revision": "bfecdeb6fa46a21ae615",
+    "url": "/irishu/static/js/2.3e1a21be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/irishu/static/js/2.3e1a21be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ca180c4dbf545727ba",
+    "url": "/irishu/static/js/main.d8e24ba1.chunk.js"
+  },
+  {
+    "revision": "420f623c0557c319f0a7",
+    "url": "/irishu/static/js/runtime-main.4de84f32.js"
+  },
+  {
+    "revision": "13f3265e49093c95e8aaabb6f29e078c",
+    "url": "/irishu/static/media/dbxgif.13f3265e.gif"
+  },
+  {
+    "revision": "62f30e3d907dde0ad3d20c92c3e9e088",
+    "url": "/irishu/static/media/headshot.62f30e3d.jpg"
+  },
+  {
+    "revision": "0e90a49c45a4a84278b4e58556c80652",
+    "url": "/irishu/static/media/linkedin.0e90a49c.png"
+  }
+]);
