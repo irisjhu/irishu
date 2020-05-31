b8f5a53ea1ede1af52b8a91e522def36e6d87ec5
Iris Hu <ihu009@gmail.com>
2020-05-31 17:30:30 -0400

    hamburger menu and font size changes for smaller screens

--- iris/src/components/headerComponent/header.js
+++ iris/src/components/headerComponent/header.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import 'hamburgers/dist/hamburgers.css';
 
 class Header extends Component {
+    componentDidMount() {
+        // Look for .hamburger
+        var hamburger = document.querySelector(".hamburger");
+        var nav = document.querySelector(".nav");
+        // On click
+        hamburger.addEventListener("click", function () {
+            // Toggle class "is-active"
+            hamburger.classList.toggle("is-active");
+            nav.classList.toggle("open");
+        });
+    }
+
     render() {
         return (
             <header>
                 <nav className="sidenav">
                     <div className="nav">
+                        <button class="hamburger hamburger--spin" type="button">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                        </button>
                         <div className="heading">
                             hi! i'm <span className="iris">iris.</span>
                         </div>
-                        <NavLink exact={true} to={process.env.PUBLIC_URL + '/'} activeClassName="active">about me</NavLink>
-                        <NavLink to={process.env.PUBLIC_URL + '/experience'} activeClassName="active">experience</NavLink>
-                        <NavLink to={process.env.PUBLIC_URL + '/projects'} activeClassName="active">projects</NavLink>
+                        <div className="menu">
+                            <NavLink exact={true} to={process.env.PUBLIC_URL + '/'} activeClassName="active">about me</NavLink>
+                            <NavLink to={process.env.PUBLIC_URL + '/experience'} activeClassName="active">experience</NavLink>
+                            <NavLink to={process.env.PUBLIC_URL + '/projects'} activeClassName="active">projects</NavLink>
+                        </div>
                     </div>
                 </nav>
             </header>
